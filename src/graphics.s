dd8be2a492eeec5470a2ae844c4cc6b5cd1642d0
Rodrigo Custodio <jorocuva@gmail.com>
2016-10-06 19:20:30 -0600

    agregado metodo reconstructor

--- src/graphics.s
+++ src/graphics.s
@@ -146,6 +146,92 @@ draw_goku:
 	bl draw_hero
 	pop {pc}
 
+@ R0, x0 a reconstruir
+@ R1, y0 a reconstruir
+@ R2, Width de la reconstruccion
+@ R3, Height de la reconstruccion
+.global reconstruct
+reconstruct:
+	push {lr}
+	mov r4, r0
+	mov r5, r1
+	mov r6, r2
+	mov r7, r3
+
+	mov r1, r4
+	mov r2, r5
+	mov r4, r6
+	mov r5, r7
+
+	x .req r1
+	y .req r2
+
+	color .req r3
+	width .req r4
+	height .req r5
+	bytecounter .req r6
+	xfinal .req r7
+	yfinal .req r8
+	temp .req r9
+	matrix_addr .req r10
+
+	mov xfinal, x
+	add xfinal, width
+	mov yfinal, y
+	add yfinal, height
+
+	mov temp, x
+	ldr matrix_addr, =Image_Matrix_bg
+	next_x1:
+		mov x, temp
+		@ Obtener l posicion inicial del bytecounter
+		push {r0-r3}
+		ldr r0, =Width_bg
+		ldr r0, [r0]
+		bl calculate_offset
+		mov bytecounter, r0 @ Guardar la pos inicial del byte counter
+		pop {r0-r3}
+		draw_pixel1:
+			cmp x, xfinal
+			bge next_y1
+			ldrh color, [matrix_addr, bytecounter]
+			ldr r0, =pixelAddr
+			ldr r0, [r0]
+			push {r0-r12}
+			bl pixel
+			pop {r0-r12}
+			add bytecounter, #2
+			add x, #1
+			b draw_pixel1
+	next_y1:
+		add y, #1
+		teq y, yfinal
+		bne next_x1
+	.unreq x
+	.unreq y
+	.unreq xfinal
+	.unreq yfinal
+	.unreq temp
+	.unreq bytecounter
+	.unreq color
+	.unreq width
+	.unreq height
+	.unreq matrix_addr
+	pop {lr}
+
+@ R0, Width
+@ R1, X
+@ R2, Y
+.global calculate_offset
+calculate_offset:
+	mov r3, #2
+	mul r0, r3, r0
+	mul r0, r2, r0
+	mul r1, r3, r1
+	add r0, r1
+	mov pc, lr
+
+
 .global wait
 wait:
 	ldr r0,=0x989680
--- src/main.s
+++ src/main.s
@@ -11,6 +11,20 @@ main:
 
 	bl draw_bg
 	render$:
+		@ldr r0, =goku_x
+		@ldr r0, [r0]
+		@ldr r1, =goku_y
+		@ldr r1, [r1]
+		@ldr r2, =Width_gok0
+		@ldr r2, [r2]
+		@ldr r3, =Height_gok0
+		@ldr r3,[r3]
+		mov r0, #50
+		mov r1, #300
+		mov r2, #66
+		mov r3, #98
+		bl reconstruct @ Reconstruir solo un pedazo del mapa
+
 		bl getkey
 		cmp r0, #'A'
 		ldreq r1, =goku_x
@@ -24,14 +38,13 @@ main:
 		ldr r1, =goku_y
 		ldr r1,[r1]
 		bl draw_goku
-		push {r0-r1}
-		bl wait
-		pop {r0-r1}
 	b render$
 	
 	
 .data
+.global goku_x
 goku_x: .word 50
+.global goku_y
 goku_y: .word 300
 chaa: .byte 'a'
 .global pixelAddr
